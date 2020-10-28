43c82a141858397db9e98d1229efb018420a2dcd
Dybeuh <57352887+Dybeuh@users.noreply.github.com>
2020-10-28 12:04:59 +0100

    Create ejercicio1.js
    Creación de la clase jugador y visualización en pantalla.

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+class Jugador {
+    constructor(nombre, ocupacion, faccion){
+        this.nombre = nombre;
+        this.ocupacion = ocupacion;
+        this.puntosVida = 100;
+        this.faccion = faccion;
+    }
+
+    toString(){
+        console.log(`Nombre: ${this.nombre}, ocupación: ${this.ocupacion}, puntos de vida: ${this.puntosVida}, facción: ${this.faccion}`)
+    }
+}
+
+a = new Jugador("Fran", "Carpintero", "Legión")
+b = new Jugador("David", "Informático", "Teemo")
+a.toString()
+b.toString()
+var listaJugadores = [a, b]
